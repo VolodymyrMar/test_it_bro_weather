23cc55772c8f03f21a3b302d69b91ec288af1d8d
Volodymyr Maruzhenko <you@example.com>
2021-09-02 20:54:59 +0300

    test

---
+++ google_api.py
@@ -0,0 +1,34 @@
+import os.path
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
+
+
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+
+SAMPLE_SPREADSHEET_ID = '1U7EdNXXZ0wIkY65VaFsGOyhLseXx_Gn65iLWYSegokw'
+SAMPLE_RANGE_NAME = 'Sheet_1'
+
+
+def make_weather_record(column, city, precipitation):
+    credentials = service_account.Credentials.from_service_account_file(
+        filename=os.path.join(
+            os.path.dirname(os.path.abspath(__file__)),
+            'credentials.json'
+        ),
+        scopes=SCOPES,
+    )
+
+    service = build('sheets', 'v4', credentials=credentials).spreadsheets().values()
+
+    service.get(
+        spreadsheetId=SAMPLE_SPREADSHEET_ID,
+        range=SAMPLE_RANGE_NAME,
+    ).execute()
+
+    # data_from_sheet = result.get('values', [])
+    service.update(
+        spreadsheetId=SAMPLE_SPREADSHEET_ID,
+        range=f'{SAMPLE_RANGE_NAME}!A{column}:B{column}',
+        valueInputOption='USER_ENTERED',
+        body={'values': [[city, precipitation]]},
+    ).execute()
---
+++ weather.py
@@ -0,0 +1,73 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+from google_api import make_weather_record
+from datetime import datetime
+
+url_wikipedia = 'https://uk.wikipedia.org/wiki/%D0%9C%D1%96%D1%81%D1%82%D0%B0_%D0%A3%D0%BA%D1%80%D0%B0%D1%97%D0%BD%D0%B8_(%D0%B7%D0%B0_%D0%BD%D0%B0%D1%81%D0%B5%D0%BB%D0%B5%D0%BD%D0%BD%D1%8F%D0%BC)'
+headers_wikipedia = {'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36',
+                     }
+token_gismeteo = {'X-Gismeteo-Token' : '605091efde0a99.85836198'}
+url_gismeteo = 'https://api.gismeteo.net/v2/weather/current/'
+city_id = 'https://api.gismeteo.net/v2/search/cities/?lang=ua&query='
+
+type_precipitation = ('Нет осадков', 'Дождь', 'Снег', 'Смешанные осадки', 'За Вашим запитом нічого не знайдено ')
+
+previous_result = {}
+
+
+def get_html():
+    response = requests.get(url_wikipedia, headers=headers_wikipedia)
+    return response
+
+
+def get_content(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    trs = soup.find('center').find_all('table')[-2].find_all('tr')
+    column = 0
+    for tr in trs[1:]:
+        link = tr.find('a')
+        if link:
+            column += 1
+            city = link.get_text()
+            current_weather = get_weather(city, headers=token_gismeteo)
+            if previous_result.get(city) != current_weather:
+                make_weather_record(
+                    column,
+                    city=city,
+                    precipitation=type_precipitation[current_weather],
+                )
+                previous_result[city] = current_weather
+
+
+def parse():
+    html = get_html()
+    if html.status_code == 200:
+        get_content(html.text)
+    else:
+        print(f'Status code {html.status_code}')
+    delay_weather_update()
+
+
+def get_id(city):
+    response = requests.get(city_id + city, headers=token_gismeteo)
+    if len(str(json.loads(response.text)['response']['items'])) > 2:
+        return str(json.loads(response.text)['response']['items'][0]['id'])
+
+
+def get_weather(city, url=url_gismeteo, headers=None):
+    if get_id(city) is not None:
+        request = requests.get(url+get_id(city), headers=headers)
+        return json.loads(request.text)['response']['precipitation']['type']
+    return 4
+
+
+def delay_weather_update():
+    current_time = datetime.now()
+    while True:
+        if (datetime.now() - current_time).seconds > 3 * 60 *60:
+            parse()
+
+
+if __name__ == '__main__':
+    parse()
