a68ff08255a7776ff2c01d34d6601fe6eb67f176
Volodymyr Maruzhenko <you@example.com>
2021-09-03 19:13:25 +0300

    addet listen_port

--- weather.py
+++ weather.py
@@ -3,6 +3,13 @@ from bs4 import BeautifulSoup
 import json
 from google_api import make_weather_record
 from datetime import datetime
+import socket, time
+from threading import Thread
+
+
+socket_ = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket_.bind(('127.0.0.1', 8080))
+socket_.listen(5)
 
 url_wikipedia = 'https://uk.wikipedia.org/wiki/%D0%9C%D1%96%D1%81%D1%82%D0%B0_%D0%A3%D0%BA%D1%80%D0%B0%D1%97%D0%BD%D0%B8_(%D0%B7%D0%B0_%D0%BD%D0%B0%D1%81%D0%B5%D0%BB%D0%B5%D0%BD%D0%BD%D1%8F%D0%BC)'
 headers_wikipedia = {'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36',
@@ -16,6 +23,21 @@ type_precipitation = ('Нет осадков', 'Дождь', 'Снег', 'Сме
 previous_result = {}
 
 
+def listen_port():
+    while True:
+        time.sleep(5)
+        (client_, address) = socket_.accept()
+        data = client_.recv(2**9)
+        if data.lower() == 'q':
+            socket_.close()
+            break
+
+        print("RECEIVED: %s" % data)
+        socket_.sendall(b'OK')
+        if data.lower() == 'q':
+            socket_.close()
+            break
+
 def get_html():
     response = requests.get(url_wikipedia, headers=headers_wikipedia)
     return response
@@ -28,9 +50,10 @@ def get_content(html):
     for tr in trs[1:]:
         link = tr.find('a')
         if link:
-            column += 1
             city = link.get_text()
             current_weather = get_weather(city, headers=token_gismeteo)
+            if current_weather != 4:
+                column += 1
             if previous_result.get(city) != current_weather:
                 make_weather_record(
                     column,
@@ -70,4 +93,7 @@ def delay_weather_update():
 
 
 if __name__ == '__main__':
+    thread = Thread(target=listen_port)
+    thread.daemon = True
+    thread.start()
     parse()
